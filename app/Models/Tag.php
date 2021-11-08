553226e70626e5c35a284b91210b8028dd8ff862
iburangulov <iburanguloff@ya.ru>
2021-11-08 11:24:52 +0500

    tags model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use DateTime;
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,11 +11,38 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tag extends Model
 {
-    use HasFactory, SoftDeletes;
+	use HasFactory, SoftDeletes;
 
 	protected $fillable = [
 		'name',
 	];
+
+	/**
+	 * @throws Exception
+	 */
+	public function getCreatedAtAttribute(string $value): string
+	{
+		return (new DateTime($value))->format('d-m-Y H:i:s');
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public function getUpdatedAtAttribute(string $value): string
+	{
+		return (new DateTime($value))->format('d-m-Y H:i:s');
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public function getDeletedAtAttribute(?string $value): ?string
+	{
+		if (!$value) {
+			return '';
+		}
+		return (new DateTime($value))->format('d-m-Y H:i:s');
+	}
 
 	/**
 	 * @return BelongsToMany
